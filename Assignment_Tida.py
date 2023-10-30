84a8f1b4d5d404a5598c0262460d49eac99e8708
Tida Dhanommitrapap <tida_d@hotmail.com>
2023-10-30 18:40:02 +1100

    The start of the guessing game.

--- Assignment_Tida.py
+++ Assignment_Tida.py
@@ -1,1 +1,9 @@
-print ("Hello World!")
+name = input("Hello! What is your name?")
+print("Hello " + name + "! My name is Tida." )
+print("Do you want to play a game? (Y/N)")
+play_game = input("Y")
+dont_play_game = input("N")
+if play_game
+    print("Let's play a guessing game!)
+if dont_play_game
+    print("Alright then. See you later!")
