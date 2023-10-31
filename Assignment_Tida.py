0ce99ef9c7e86739d2a7bc4f14ad8f65cdb6e131
Tida Dhanommitrapap <tida_d@hotmail.com>
2023-10-31 21:35:57 +1100

    Finished!

--- Assignment_Tida.py
+++ Assignment_Tida.py
@@ -3,10 +3,14 @@ print("Hello " + player_name + "! My name is Tida." )
 answer = input("Do you want to play a game? (Y/N) ")
 if answer == "Y":
     print ("Let's play a guessing game!")
-    fave_colour = input("Can you guess my favourite colour? ")
-    if fave_colour == "Purple":
-        print ("That's right! Congratulations " + player_name + "!")
-    if fave_colour != "Purple":
-        print("Try again " + player_name + ".")
+    fave_colour = "Purple"
+    guess = input("Can you guess my favourite colour? ")
+    while True:
+        if guess != fave_colour:
+            print("Try again " + player_name + ".")
+            guess = input("Can you guess my favourite colour? ")
+        elif guess == fave_colour:
+                break
+    print ("That's right! Congratulations " + player_name + "!")
 if answer == "N":
     print("Alright then. See you later!")
