dc768d66dac67d7d4dc7776c6d8980f4c5390eb1
tidanette <tida_d@hotmail.com>
2023-11-07 20:14:40 +1100

    Changed so that responses can be in lower changes Added the ability to have the number of guesses

--- Assignment_Tida.py
+++ Assignment_Tida.py
@@ -1,16 +1,18 @@
 player_name = input("Hello! What is your name? ")
 print("Hello " + player_name + "! My name is Tida." )
 answer = input("Do you want to play a game? (Y/N) ")
-if answer == "Y":
+if answer.lower() == "y":
     print ("Let's play a guessing game!")
-    fave_colour = "Purple"
+    fave_colour = "purple"
+    no_guesses = 1
     guess = input("Can you guess my favourite colour? ")
     while True:
-        if guess != fave_colour:
+        if guess.lower() != fave_colour:
             print("Try again " + player_name + ".")
+            no_guesses += 1
             guess = input("Can you guess my favourite colour? ")
-        elif guess == fave_colour:
+        elif guess.lower() == fave_colour.lower():
                 break
-    print ("That's right! Congratulations " + player_name + "!")
-if answer == "N":
+    print ("That's right! Congratulations " + player_name + "! It took you " + ,no_guesses,"guesses.")
+if answer.lower() == "n":
     print("Alright then. See you later!")
